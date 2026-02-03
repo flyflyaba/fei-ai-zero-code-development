18f1e6dfa2ff1dce1790dbbd24d665165b334bcb
soar <1147358383@qq.com>
2026-02-03 23:20:11 +0800

    第4次提交 - AI生成网页应用 1. 通过LangChain4j接入大模型 2. 实现生成单文件（html）和多文件（html、css、js）代码 3. 实现SSE流式输出网页代码 4. 实现代码生成结果保存到本地文件系统 5. 通过策略模式、模版方法模式、执行器模式优化代码结构

---
+++ src/main/java/com/fei/feiaizerocodedevelopment/ai/AiCodeGeneratorService.java
@@ -0,0 +1,46 @@
+package com.fei.feiaizerocodedevelopment.ai;
+
+import com.fei.feiaizerocodedevelopment.ai.model.HtmlCodeResult;
+import com.fei.feiaizerocodedevelopment.ai.model.MultiFileCodeResult;
+import dev.langchain4j.service.SystemMessage;
+import reactor.core.publisher.Flux;
+
+public interface AiCodeGeneratorService {
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    HtmlCodeResult generateHtmlCode(String userMessage);
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    MultiFileCodeResult generateMultiFileCode(String userMessage);
+
+    /**
+     * 生成 HTML 代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-html-system-prompt.txt")
+    Flux<String> generateHtmlCodeStream(String userMessage);
+
+    /**
+     * 生成多文件代码
+     *
+     * @param userMessage 用户提示词
+     * @return AI的输出结果
+     */
+    @SystemMessage(fromResource = "prompt/codegen-multi-file-system-prompt.txt")
+    Flux<String> generateMultiFileCodeStream(String userMessage);
+
+}
