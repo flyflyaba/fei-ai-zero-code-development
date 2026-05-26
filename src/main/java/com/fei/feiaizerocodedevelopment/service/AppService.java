709cebecf2b262e29c4e9c24a7aaa46420df7ce9
soar <1147358383@qq.com>
2026-05-26 23:22:20 +0800

    第5次提交 - 应用模块（完成基本功能） 1. AI 生成基础功能代码 2. 完成基本功能，人工测试通过

--- src/main/java/com/fei/feiaizerocodedevelopment/service/AppService.java
+++ src/main/java/com/fei/feiaizerocodedevelopment/service/AppService.java
@@ -1,7 +1,12 @@
 package com.fei.feiaizerocodedevelopment.service;
 
+import com.fei.feiaizerocodedevelopment.model.dto.app.AppQueryRequest;
+import com.fei.feiaizerocodedevelopment.model.vo.AppVO;
+import com.mybatisflex.core.query.QueryWrapper;
 import com.mybatisflex.core.service.IService;
 import com.fei.feiaizerocodedevelopment.model.entity.App;
+
+import java.util.List;
 
 /**
  * 应用 服务层。
@@ -10,4 +15,27 @@ import com.fei.feiaizerocodedevelopment.model.entity.App;
  */
 public interface AppService extends IService<App> {
 
+    /**
+     * 获取应用封装类
+     *
+     * @param app
+     * @return
+     */
+    AppVO getAppVO(App app);
+
+    /**
+     * 批量获取应用封装类列表
+     *
+     * @param appList
+     * @return
+     */
+    List<AppVO> getAppVOList(List<App> appList);
+
+    /**
+     * 构造应用查询条件
+     *
+     * @param appQueryRequest
+     * @return
+     */
+    QueryWrapper getQueryWrapper(AppQueryRequest appQueryRequest);
 }
